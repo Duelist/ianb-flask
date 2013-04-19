13740668a9570ca824c59a2a7ba204bfc2ecb94e
Ian Benedict <ianbenedict@gmail.com>
2013-04-19 07:39:55 -0400

    Committing existing files

---
+++ ianb/settings.py
@@ -0,0 +1,10 @@
+envir = {
+  
+}
+
+context = {
+  'version': '0.01',
+  'title': 'Ian Benedict',
+  'twitter_username': 'Duelist',
+  'github_username': 'Duelist',
+}
---
+++ ianb/views.py
@@ -0,0 +1,19 @@
+from ianb import app
+from ianb.settings import *
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html', context=context)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', context=context)
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', context=context)
+
+@app.errorhandler(404)
+def error404(error):
+    return render_template('404.html', context=context), 404
